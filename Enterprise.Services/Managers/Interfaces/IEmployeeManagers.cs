4240d90035a767c3d5597dd85067421464fe5aea
ShubhamKumar <shubhamk@saxon.ai>
2024-12-05 18:04:25 +0530

    added some file

---
+++ Enterprise.Services/Managers/Interfaces/IEmployeeManagers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enterprise.Services.Managers.Interfaces
+{
+    public interface IEmployeeManagers
+    {
+    }
+}
